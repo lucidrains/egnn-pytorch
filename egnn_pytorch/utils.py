926addbd56c6c45ceede11ddf26b2a9989e29728
Phil Wang <lucidrains@gmail.com>
2021-02-27 06:56:42 -0800

    fix tests

---
+++ egnn_pytorch/utils.py
@@ -0,0 +1,19 @@
+import torch
+from torch import sin, cos, atan2, acos
+
+def rot_z(gamma):
+    return torch.tensor([
+        [cos(gamma), -sin(gamma), 0],
+        [sin(gamma), cos(gamma), 0],
+        [0, 0, 1]
+    ], dtype=gamma.dtype)
+
+def rot_y(beta):
+    return torch.tensor([
+        [cos(beta), 0, sin(beta)],
+        [0, 1, 0],
+        [-sin(beta), 0, cos(beta)]
+    ], dtype=beta.dtype)
+
+def rot(alpha, beta, gamma):
+    return rot_z(alpha) @ rot_y(beta) @ rot_z(gamma)
