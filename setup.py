d3ba97d5b2540085cf8e02c838ebf5e42071697f
lucidrains <lucidrains@gmail.com>
2024-12-06 06:22:57 -0800

    0.2.8

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'egnn-pytorch',
   packages = find_packages(),
-  version = '0.2.7',
+  version = '0.2.8',
   license='MIT',
   description = 'E(n)-Equivariant Graph Neural Network - Pytorch',
   long_description_content_type = 'text/markdown',
